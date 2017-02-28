cd48fb25feed3c7c5d04f71204d6a4596b5bf2d1
elaine lim <lainex@gmail.com>
2017-02-28 09:53:23 +0800

    completed all

--- lib/02letter_count.rb
+++ lib/02letter_count.rb
@@ -8,5 +8,17 @@
 # > {"b" => 1, "a" => 3, "n" => 2}
 
 def letter_count(str)
-
+letter = {}
+i = 0
+while i < str.length
+  if letter.key?str[i]
+    letter[str[i]] += 1
+  else
+    letter[str[i]] = 1
+  end
+    i += 1
+  end
+  p letter
 end
+
+letter_count('banana')
